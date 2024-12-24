0db2c5aa162179058bae8527b1eea22256e3d8ef
haris <muhammadhafiz2102@gmail.com>
2024-12-24 14:37:43 +0700

    Initialization Project

--- LatihanGit/src/tm/pkg12/gitaritmatika/MainApp.java
+++ LatihanGit/src/tm/pkg12/gitaritmatika/MainApp.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package latihangit;
+package tm.pkg12.gitaritmatika;
 
 /**
  *
  * @author LENOVO
  */
-public class LatihanGit {
+public class MainApp {
 
     /**
      * @param args the command line arguments
